48ac6823802341117ad9a9a6cb6aaeac6124ff7f
MathDEV-0 <teteuviegas.2006@hotmail.com>
2026-01-09 17:29:27 -0300

    launch: first commit

---
+++ src/test/java/com/math_dev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math_dev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
