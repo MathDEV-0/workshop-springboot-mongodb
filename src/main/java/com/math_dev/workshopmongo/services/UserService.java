0a8ee0233ec661704974eade68d21defae1cdb14
MathDEV-0 <teteuviegas.2006@hotmail.com>
2026-02-19 18:08:36 -0300

    Inserção de usuário com método POST

--- src/main/java/com/math_dev/workshopmongo/services/UserService.java
+++ src/main/java/com/math_dev/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.math_dev.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.math_dev.workshopmongo.domain.User;
+import com.math_dev.workshopmongo.dto.UserDTO;
 import com.math_dev.workshopmongo.repositories.UserRepository;
+import com.math_dev.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +22,25 @@ public class UserService {
 	    return list;
 	}
 
+	public User findById(String id){
+	    Optional<User> user = userRepo.findById(id);
+
+	    if(user.isEmpty()) {
+	    	System.out.println("NÃO ACHOU!");
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+
+	    return user.get();
+	}
+	
+	//Dependendo da situação, para instanciar um User, posso querer acessar o DB;
+	//Como UserService já tem acesso, para evitar manutenção posterior o insert foi criado ...
+	//No UserService, e não no UserDTO
+	public User insert(User obj) {
+		return userRepo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
 }
