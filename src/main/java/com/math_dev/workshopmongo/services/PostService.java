3c2437eaa02c65827c409eda2d42c83b2c499f70
MathDEV-0 <teteuviegas.2006@hotmail.com>
2026-02-20 19:11:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/math_dev/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.math_dev.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.math_dev.workshopmongo.domain.Post;
+import com.math_dev.workshopmongo.repositories.PostRepository;
+import com.math_dev.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository postRepo;
+	
+	
+	public Post findById(String id){
+	    Optional<Post> post = postRepo.findById(id);
+
+	    if(post.isEmpty()) {
+	    	System.out.println("NÃO ACHOU!");
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+
+	    return post.get();
+	}
+	
+	
+}
