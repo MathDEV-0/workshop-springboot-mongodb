2b10ef771442e4f53ed000e5a281ef6f26789a03
MathDEV-0 <teteuviegas.2006@hotmail.com>
2026-02-20 19:20:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/math_dev/workshopmongo/domain/Post.java
+++ src/main/java/com/math_dev/workshopmongo/domain/Post.java
@@ -1,9 +1,12 @@
 package com.math_dev.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.math_dev.workshopmongo.dto.AuthorDTO;
+import com.math_dev.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable{
 	
@@ -15,6 +18,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -68,6 +72,16 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+	
 	
 	
 }
